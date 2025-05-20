995d2b0b6686d98aed6caf90bc3e633bc179ffe6
maira <syeda.maira@xynotech.com>
2025-05-20 16:21:59 +0500

    Implement Complete and Exit

--- GUI.py
+++ GUI.py
@@ -5,9 +5,14 @@ input_box=sg.InputText(tooltip="Enter TODO", key='todo')
 add_btn = sg.Button('Add')
 todos_list = sg.Listbox(values=functions.read_todos() , key='todos',enable_events=True, size =[45 , 10])
 edit_btn = sg.Button('Edit')
+complete_btn = sg.Button('Complete')
+exit_btn = sg.Button('Exit')
 
 window = sg.Window('My TODO App',
-                   layout=[[label],[input_box,add_btn],[todos_list,edit_btn]],
+                   layout=[[label],
+                           [input_box,add_btn],
+                           [todos_list,edit_btn, complete_btn],
+                           [exit_btn]],
                    font=('Helevica',20))
 while True:
     event, values = window.read()
@@ -27,8 +32,17 @@ while True:
             todos[todo_index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case 'Complete':
+            todo = values['todos'][0]
+            todos = functions.read_todos()
+            todos.remove(todo)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
         case 'todos':
             window['todo'].update(value=values['todos'][0])
+        case 'Exit':
+            break
         case sg.WIN_CLOSED:
             break
 window.close()
